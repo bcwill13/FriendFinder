b816ecdd4e41e50ddfef58a8ec19f9e6d965bda5
bcwill13 <50090984+bcwill13@users.noreply.github.com>
2019-08-15 16:26:18 -0400

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+var friends = [{
+    name: "Dale",
+    photo: "file:///Users/Brigit/Downloads/FriendFinder/app/public/images/character1.png",
+    scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
+  }, {
+    name: "Jago",
+    photo: "/Users/Brigit/Downloads/FriendFinder/app/public/images/character2.png",
+    scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
+  }, {
+    name: "Axl",
+    photo: "/Users/Brigit/Downloads/FriendFinder/app/public/images/character3.png",
+    scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
+  },{
+    name: "Carson",
+    photo: "/Users/Brigit/Downloads/FriendFinder/app/public/images/character4.png",
+    scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
+  }, {
+    name: "Roland",
+    photo: "/Users/Brigit/Downloads/FriendFinder/app/public/images/character6.png",
+    scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
+  }, {
+    name: "Maxime",
+    photo: "/Users/Brigit/Downloads/FriendFinder/app/public/images/character5.png",
+    scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+  }, {
+    name: "Nicola",
+    photo: "/Users/Brigit/Downloads/FriendFinder/app/public/images/character7.png",
+    scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
+  }];
+    
+  module.exports = friends;
